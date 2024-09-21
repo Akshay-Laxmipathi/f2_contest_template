c1d1fb3a670cc822accd27e6d8939340dccc47ca
Akshay-Laxmipathi <akshay.nighthawk@gmail.com>
2024-09-22 01:38:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,55 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+let employee = { id: 4, name: "susan", age: "20", profession: "intern" };
+
+let arr2 = [
+  { id: 5, name: "jane", age: "21", profession: "developer" },
+  { id: 6, name: "jill", age: "23", profession: "developer" },
+  { id: 7, name: "charles", age: "22", profession: "admin" },
+];
+
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  console.log(
+    arr.map((x) => {
+      if (x.profession == "developer") {
+        return x;
+      }
+    })
+  );
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  console.log(
+    arr.forEach((x) => {
+      if (x.profession == "developer") {
+        return x;
+      }
+    })
+  );
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr = [...arr, employee];
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  console.log(
+    arr.filter((x) => {
+      if (x.profession != "admin") {
+        return x;
+      }
+    })
+  );
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let cc = arr.concat(arr2);
+  console.log(cc);
 }
